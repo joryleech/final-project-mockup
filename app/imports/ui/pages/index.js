ce0319931ed2f532525510eb9d480cbdec2484be
Jory Leech <joryleech@gmail.com>
2017-03-20 21:23:07 -1000

    Finished the mockup

---
+++ app/imports/ui/pages/index.js
@@ -0,0 +1,18 @@
+import './add-stuff-page.html';
+import './add-stuff-page.js';
+
+import './edit-stuff-page.html';
+import './edit-stuff-page.js';
+
+import './home-page.html';
+
+import './list-stuff-page.html';
+import './list-stuff-page.js';
+
+import './about-us-page.html';
+
+import './view-my-campaigns-page.html'
+
+import './view-campaigns.html'
+
+import './add-campaign.html'
